c2da3ed8b89dab15f8ee9e91e8aa1b95edbcf610
WoojuHwww <qpzm121206@gmail.com>
2026-05-10 15:36:52 +0900

    fix : update Github action s3

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "EDF-terraform-state-398875891485"
+    bucket         = "my-project-terraform-state-398875891485-teamedf"
     key            = "dev/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true
